e5f011802362b2bc3814342a953f0fdacdbc948c
Claudio Alejo <calejoalar@gmail.com>
2024-09-14 00:48:52 -0500

    learn declare variables, array, map, if

---
+++ swiftTour/Sources/main.swift
@@ -0,0 +1,45 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+/*****/
+let myConstant = 44
+var myVariable = 12
+myVariable = 39
+print(myConstant)
+print(myVariable)
+
+/*****/
+
+var test01: Float = 23.33
+var test02: String = "hola"
+var test03: [String] = []
+var test04: [String: Float] = [:]
+
+/*****/
+
+let individualScore = [23,66,57,23,99,102]
+var teamScore = 0;
+for score in individualScore {
+    if score >= 50 {
+        teamScore += 3
+    } else {
+        teamScore += 1
+    }
+}
+print(teamScore)
+
+/*****/
+
+print("Hello, world!")
+
+var colors = ["red", "yellow", "green"]
+colors.append("black")
+
+print(colors)
+
+var book = [
+    "author": "Michael Phelps",
+    "price": "23.54"
+]
+
+print(book)
